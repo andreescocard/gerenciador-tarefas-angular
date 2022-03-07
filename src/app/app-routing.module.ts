aaa0303303cd04404f343e295d017fc75a73844d
André <andre.escocard@hesea.com.br>
2022-03-07 12:31:27 -0300

    init repo

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)], //singleton
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/tarefas/shared/tarefa.model.ts
@@ -0,0 +1,7 @@
+export class Tarefa {
+
+	constructor(
+		public id?: number, //? - atributos n obrigatorios
+		public nome?: string,
+		public concluida?: boolean) {}
+}
