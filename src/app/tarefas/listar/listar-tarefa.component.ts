7d903683b12c169798ea17c4f4ed071286723f32
André <andre.escocard@nttdata.com>
2022-03-22 11:16:55 -0300

    feat: task listing

--- src/app/tarefas/listar/listar-tarefa.component.ts
+++ src/app/tarefas/listar/listar-tarefa.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TarefaService, Tarefa } from '../shared';
+
 @Component({
   selector: 'app-listar-tarefa',
   templateUrl: './listar-tarefa.component.html',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarTarefaComponent implements OnInit {
 
-  constructor() { }
+  tarefas: Tarefa[];
+
+  constructor(private tarefaService: TarefaService) { }
 
   ngOnInit(): void {
+    this.tarefas = this.listarTodos();
+  }
+
+  listarTodos(): Tarefa[] {
+  	return this.tarefaService.listarTodos();
+  }
+
+  remover($event: any, tarefa: Tarefa): void {
+    $event.preventDefault();
+    if (confirm('Deseja remover a tarefa "' + tarefa.nome + '"?')) {
+      this.tarefaService.remover(tarefa.id!);
+      this.tarefas = this.tarefaService.listarTodos();
+    }
+  }
+
+  alterarStatus(tarefa: Tarefa): void {
+    if (confirm('Deseja alterar o status da tarefa "' + tarefa.nome + '"?')) {
+      this.tarefaService.alterarStatus(tarefa.id!);
+      this.tarefas = this.tarefaService.listarTodos();
+    }
   }
 
 }
